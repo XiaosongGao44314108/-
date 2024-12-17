843b1583363d139858a445ad88ba876afcfa94ec
XiaosongGao44314108 <xiaosong.gao@students.mq.edu.au>
2024-12-17 18:45:13 +0800

    问题一解决方案一

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,17 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public GameObject ExamplePanel;
+    public TextMeshProUGUI textComponent;
 
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            TextsolutionOne();
+        }
     }
 
-    public void CheckTextResult(){
+    public void TextsolutionOne()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 localPoint;
+ 
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(textComponent.rectTransform, mousePos, null, out localPoint))
+        {
+            Vector2 textSize = textComponent.rectTransform.sizeDelta;
+            Vector2 textPos = textComponent.rectTransform.anchoredPosition;
+ 
+            if (localPoint.x > textPos.x - textSize.x / 2 && localPoint.x < textPos.x + textSize.x / 2 &&
+                    localPoint.y > -textPos.y - textSize.y / 2 && localPoint.y < -textPos.y + textSize.y / 2)
+            {
+                CheckTextResult();
+            }    
+
+        }
+    }
+
+    public void CheckTextResult()
+    {
         Debug.Log("Text has been clicked");
     }
+
+    public void CheckOResult()
+    {
+        Debug.Log("O has been clicked");
+    }
+
 }
