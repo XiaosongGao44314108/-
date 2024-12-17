f0c6bfead3ae84a66abad5ce819c3e107328c093
XiaosongGao44314108 <xiaosong.gao@students.mq.edu.au>
2024-12-17 15:52:14 +0800

    Initial commit

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject ExamplePanel;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckTextResult(){
+        Debug.Log("Text has been clicked");
+    }
+}
